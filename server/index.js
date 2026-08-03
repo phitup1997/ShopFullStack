edb8343decc5a0166a12502426af437af19ab16f
phi <quangphi@dnkinno.com>
2026-08-03 15:54:07 +0700

    feat: apply cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
-const express = require("express")
-require("dotenv").config()
-const dbconnection = require("./config/dbconnection")
-const initRoutes = require("./routes")
-const cookieParser = require("cookie-parser")
+const express = require('express')
+require('dotenv').config()
+const dbconnection = require('./config/dbconnection')
+const initRoutes = require('./routes')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 const app = express()
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['POST', 'GET', 'PUT', 'DELETE'],
+  }),
+)
 const port = process.env.PORT || 8888
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -12,10 +19,10 @@ app.use(cookieParser())
 dbconnection()
 
 initRoutes(app)
-app.use("/", (req, res) => {
-  res.send("SERVER IS ONNN")
+app.use('/', (req, res) => {
+  res.send('SERVER IS ONNN')
 })
 
 app.listen(port, () => {
-  console.log("Server running on the part ", port)
+  console.log('Server running on the part ', port)
 })
--- server/routes/productCategory.js
+++ server/routes/productCategory.js
@@ -1,11 +1,11 @@
-const controller = require("../controllers/productCategory")
-const router = require("express").Router()
-const { verifyToken, isAdmin } = require("../middleware/verifyToken")
+const controller = require('../controllers/productCategory')
+const router = require('express').Router()
+const { verifyToken, isAdmin } = require('../middleware/verifyToken')
 
+router.get('/', controller.getProductCategories)
 router.use(verifyToken)
-router.get("/", controller.getProductCategories)
-router.post("/create", isAdmin, controller.createProductCategory)
-router.delete("/delete", isAdmin, controller.deleteProductCategory)
-router.post("/update", isAdmin, controller.updateProductCategory)
+router.post('/create', isAdmin, controller.createProductCategory)
+router.delete('/delete', isAdmin, controller.deleteProductCategory)
+router.post('/update', isAdmin, controller.updateProductCategory)
 
 module.exports = router
